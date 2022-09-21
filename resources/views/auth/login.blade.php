59fa035b0a64163cc37ad2a6018cabcc640a30af
narinder singh <narindersmartitventures@gmail.com>
2022-09-21 18:31:21 +0530

    custom cahnges

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Login</title>
-        <link rel="icon" href="{{ asset('images/moblogo.png') }}" type="image/png" sizes="16x16" />
+        <link rel="icon" href="{{ asset('images/smartit.png') }}" type="image/png" sizes="16x16" />
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
         <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
         <link href="{{ asset('css/responsive.css') }}" rel="stylesheet" />
@@ -18,19 +18,16 @@
                             <div class="leftloginwrapper">
                                 <div class="leftloginformcontainer">
                                     <div class="leftformsectionboxheading">
-                                        <img src="{{ asset('images/mainlogo3.png') }}" />
+                                        <!-- <img src="{{ asset('images/smart_it.png') }}" /> -->
                                     </div>
                                     <div class="leftformsectioninnerdata">
-                                        <h1>Welcome to <span>Cleaning Service</span></h1>
-                                        <h6>Please sign in to continue</h6>
+                                        <h1>Welcome to <span> Smart It Ventures</span></h1>
                                         <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
                                             @csrf
                                             <div class="row">
                                                 <div class="col-md-12">
                                                     <div class="form-group">
-                                                        <div class="iconbox">
-                                                            <i class="fa fa-envelope-o" aria-hidden="true"></i>
-                                                        </div>
+                                                        
                                                         <input type="text" name="email" class="form-control customchangesemail email-input {{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus placeholder="Enter email" />
 
                                                         @if ($errors->has('email'))
@@ -42,9 +39,7 @@
                                                 </div>
                                                 <div class="col-md-12">
                                                     <div class="form-group">
-                                                        <div class="iconbox">
-                                                            <i class="fa fa-unlock-alt" aria-hidden="true"></i>
-                                                        </div>
+                                                       
                                                         <input type="Password" name="password" class="form-control customchangesemail password-input {{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required placeholder="Enter password" />
 
                                                         <div class="iconbox2">
@@ -67,7 +62,7 @@
                         </div>
                         <div class="col-md-7">
                             <div class="leftformsectionbox">
-                                <img src="{{ asset('images/artistmainpic.png') }}" />
+                                <img src="{{ asset('images/smart_it.png') }}" />
                             </div>
                         </div>
                     </div>
