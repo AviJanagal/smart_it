d32a9fc060ecb6d5e84072ee98a9d7363daf35fa
AviJanagal <avinashsmartitventures@gmail.com>
2022-10-20 10:13:33 +0530

    chnages on employee pannel

--- app/Http/Controllers/employee/EmployeeController.php
+++ app/Http/Controllers/employee/EmployeeController.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Controllers\employee;
 use App\Http\Controllers\Controller;
-use Auth;
-use Mail;
 use App\Mail\ApplyLeaveMail;
-use DateTime;
-use DateInterval;
-use DatePeriod;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
-use PDF;
+use PDF,DB,DateTime,DateInterval,DatePeriod,Mail,Auth;
 
 use Illuminate\Http\Request;
 
@@ -193,38 +188,45 @@ class EmployeeController extends Controller
 
     public function attendance_filter(Request $request){
         $year = $request->year;
-        if(!empty($request->month)){
-            $umonth = $request->month;
-            $month = date("m", strtotime($umonth));
-        }
-        if(!empty($request->date)){
-            $date = date("Y-m-d", strtotime($request->date));
-        }
+        (!empty($request->month))?  $month = date("m", strtotime($request->month)):  $month = "" ;
+        (!empty($request->date))? $date = date("Y-m-d", strtotime($request->date)) : $date = "" ;
+       
+        $my_attendance = \App\EmployeeAttendance::select("*")
+            ->when(!empty($year), function ($query) use ($year) {
+                $query->where('employee_id',Auth::id())->whereYear('created_at',$year)->get();
+            })
+            ->when(!empty($month), function ($query) use ($month) {
+                $query->where('employee_id',Auth::id())->whereMonth('created_at', $month)->get();
+            })
+            ->when(!empty($date), function ($query) use ($date) {
+                $query->where('employee_id',Auth::id())->whereDate('created_at',$date)->get();
+            })
+            ->get();
      
-        if(!empty($year) && !empty($month) && !empty($date)){
-             $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereYear('created_at',$year)->whereMonth('created_at', $month)->whereDate('created_at',$date)->get();
-        }
-        elseif(!empty($year) && !empty($month) && empty($date)){
-             $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereYear('created_at',$year)->whereMonth('created_at', $month)->get();
-        }
-        elseif(!empty($year) && empty($month) && empty($date)){
-             $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereYear('created_at',$year)->get();
-        }
-        elseif(!empty($year) && empty($month) && !empty($date)){
-             $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereYear('created_at',$year)->whereDate('created_at',$date)->get();
-        }
-        elseif(empty($year) && !empty($month) && !empty($date)){
-             $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereMonth('created_at', $month)->whereDate('created_at',$date)->get();
-        }
-        elseif(empty($year) && !empty($month) && empty($date)){
-            $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereMonth('created_at', $month)->get();
-        }
-        elseif(empty($year) && empty($month) && !empty($date)){
-             $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereDate('created_at',$date)->get();
-        }
-        else{
-            $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereMonth('created_at', \Carbon\Carbon::now()->month)->get();
-        }
+        // if(!empty($year) && !empty($month) && !empty($date)){
+        //      $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereYear('created_at',$year)->whereMonth('created_at', $month)->whereDate('created_at',$date)->get();
+        // }
+        // elseif(!empty($year) && !empty($month) && empty($date)){
+        //      $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereYear('created_at',$year)->whereMonth('created_at', $month)->get();
+        // }
+        // elseif(!empty($year) && empty($month) && empty($date)){
+        //      $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereYear('created_at',$year)->get();
+        // }
+        // elseif(!empty($year) && empty($month) && !empty($date)){
+        //      $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereYear('created_at',$year)->whereDate('created_at',$date)->get();
+        // }
+        // elseif(empty($year) && !empty($month) && !empty($date)){
+        //      $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereMonth('created_at', $month)->whereDate('created_at',$date)->get();
+        // }
+        // elseif(empty($year) && !empty($month) && empty($date)){
+        //     $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereMonth('created_at', $month)->get();
+        // }
+        // elseif(empty($year) && empty($month) && !empty($date)){
+        //      $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereDate('created_at',$date)->get();
+        // }
+        // else{
+        //     $my_attendance = \App\EmployeeAttendance::where('employee_id',Auth::id())->whereMonth('created_at', \Carbon\Carbon::now()->month)->get();
+        // }
         $year_status = $request->year;
         $month_status = $request->month;
         $date_status = $request->date;
@@ -235,13 +237,34 @@ class EmployeeController extends Controller
 
     public function activity_filter(Request $request){
         $year = $request->year;
-        if(!empty($request->month)){
-            $umonth = $request->month;
-            $month = date("m", strtotime($umonth));
-        }
-        if(!empty($request->date)){
-            $date = date("Y-m-d", strtotime($request->date));
-        }
+        (!empty($request->month))?  $month = date("m", strtotime($request->month)):  $month = "" ;
+        (!empty($request->date))? $date = date("Y-m-d", strtotime($request->date)) : $date = "" ;
+
+        // return $all_daily_activities = \App\DailyActivity::select(DB::raw("daily_activities.* "))
+        //     ->when(!empty($year), function ($query) use ($year) {
+        //         $query->where('employee_id',Auth::id())->whereYear('created_at',$year)->get();
+        //     })
+        //     ->when(!empty($month), function ($query) use ($month) {
+        //         $query->where('employee_id',Auth::id())->whereMonth('created_at', $month)->get();
+        //     })
+        //     ->when(!empty($date), function ($query) use ($date) {
+        //         $query->where('employee_id',Auth::id())->whereDate('created_at',$date)->get();
+        //     })
+        //     ->sum('time_in_minutes');
+
+        //     $total_mins = \App\DailyActivity::select("*")
+        //     ->when(!empty($year), function ($query) use ($year) {
+        //         $query->where('employee_id',Auth::id())->whereYear('created_at',$year)->where('project_id','!=',0)->sum('time_in_minutes');
+        //     })
+        //     ->when(!empty($month), function ($query) use ($month) {
+        //         $query->where('employee_id',Auth::id())->whereMonth('created_at', $month)->where('project_id','!=',0)->sum('time_in_minutes');
+        //     })
+        //     ->when(!empty($date), function ($query) use ($date) {
+        //         $query->where('employee_id',Auth::id())->whereDate('created_at',$date)->where('project_id','!=',0)->sum('time_in_minutes');
+        //     })
+        //     ->where('project_id','!=',0)->sum('time_in_minutes');
+
+     
      
         if(!empty($year) && !empty($month) && !empty($date)){
              $all_daily_activities = \App\DailyActivity::where('employee_id',Auth::id())->whereYear('created_at',$year)->whereMonth('created_at', $month)->whereDate('created_at',$date)->get();
